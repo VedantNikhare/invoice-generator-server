8fc7c02f7896b06a7c07904dcca9f0040f36b22b
Vedant Nikhare <98038580+VedantNikhare@users.noreply.github.com>
2024-02-02 21:38:10 +0530

    ab index.js

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ app.use(cors({
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      'https://arc-invoice-tool.vercel.app',
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 
 app.use('/invoices', invoiceRoutes)
 app.use('/clients', clientRoutes)
